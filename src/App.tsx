e2b7f6a2b09648c88215fc9f8e00d7b33d38ad0a
Magaly <fernandezgarciamagaly@gmail.com>
2025-08-07 12:06:08 +0200

    adding animation on the dward to reveal the site

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import DwarfBubble from "./DwarfBubble";
 
 function App() {
   return (
-    <main className="h-full  bg-amber-400 justify-center items-center flex">
+    <main className="h-full bg-amber-400 justify-center items-center flex">
       <DwarfBubble />
     </main>
   );
--- src/DwarfBubble.tsx
+++ src/DwarfBubble.tsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
+
 export default function DwarfBubble() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="flex flex-col w-120">
-      <div className="relative bg-white text-black p-4 rounded-xl shadow-lg w-fit self-end">
-        <div className="text-2xl ml">Chaussettes !!!</div>
-        <div className="absolute -bottom-1 left-1 w-4 h-4 bg-white rotate-70"></div>
+    <div className="relative w-screen h-screen overflow-hidden">
+      <div
+        className={`absolute inset-0 z-0 p-10 flex flex-col items-center justify-center transition-all duration-500 ease-in-out`}
+      >
+        <h1 className="text-4xl font-bold">Welcome to socks land !</h1>
+        <p>Where all socks' dreams come true</p>
       </div>
-      <img src="/nain.png" className="w-100" />
+
+      <div
+        className={`absolute z-10 p-6 rounded-lg transition-all duration-500 ease-in-out flex items-center justify-center  bg-amber-400
+          ${
+            isHovered ? "w-40 h-fit top-4 left-4" : "w-full h-full top-0 left-0"
+          }
+        `}
+      >
+        <div className="flex flex-col w-120 ">
+          <div
+            className={`relative bg-white text-black p-4 rounded-xl shadow-lg transition-all duration-500 ease-in-out  ${
+              isHovered ? "hidden" : "self-end"
+            }`}
+          >
+            <div className="text-2xl ml">Chaussettes !!!</div>
+            <div className="absolute -bottom-1 left-1 w-4 h-4 bg-white rotate-70"></div>
+          </div>
+          <img
+            src="/nain.png"
+            className={`transition-all duration-500 ease-in-out${
+              isHovered ? "w-20 self-center" : "w-100"
+            }`}
+            onMouseEnter={() => setIsHovered((prev) => !prev)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
